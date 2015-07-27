3877cbc2a86a8fa0bb64bc72bf22a4023af13a24
Tom Bremer <tom@tbremer.com>
2015-07-27 14:44:58 -0500

    initial-push add mocha-phantomjs for tests, update eslint for mocha and globals

--- tests/test.js
+++ tests/test.js
@@ -11,7 +11,8 @@ describe('Collector.js', function() {
     describe('hasClass', function() {
       it('should return false', function() {
         expect($('#collector-hasClass-false div').hasClass('test')).to.equal(false);
-      })
+      });
+
       it('should return true', function() {
         expect($('#collector-hasClass-true div').hasClass('test')).to.equal(true);
       });
