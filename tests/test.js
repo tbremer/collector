a72bb3b2acbbfe928be79ae2b39d7959b6771859
Tom Bremer <tom@tbremer.com>
2015-07-27 02:22:06 -0500

    initial-push: add some tests for the Class and selector api

--- tests/test.js
+++ tests/test.js
@@ -1,12 +1,39 @@
-'use strict';
+var expect = chai.expect;
 
-let assert = require("assert");
+describe('Collector.js', function() {
+  describe('$', function() {
+    it('should grab nodes', function() {
+      expect($('#collector-selection .test').length).to.equal(6);
+    });
+  });
 
-describe('Array', function() {
-  describe('#indexOf()', function () {
-    it('should return -1 when the value is not present', function () {
-      assert.equal(-1, [1,2,3].indexOf(5));
-      assert.equal(-1, [1,2,3].indexOf(0));
+  describe('Class API', function() {
+    describe('hasClass', function() {
+      it('should return false', function() {
+        expect($('#collector-hasClass-false div').hasClass('test')).to.equal(false);
+      })
+      it('should return true', function() {
+        expect($('#collector-hasClass-true div').hasClass('test')).to.equal(true);
+      });
+    });
+
+    describe('addClass', function() {
+      it('should add the class `active`', function() {
+        $('#collector-addClass .test').addClass('active');
+
+        expect(document.querySelector('#collector-addClass .test').className).to.equal("test active");
+      });
+
+      it('should not duplicate the class `active`', function() {
+        $('#collector-addClass .test').addClass('active');
+        $('#collector-addClass .test').addClass('active');
+        $('#collector-addClass .test').addClass('active');
+        $('#collector-addClass .test').addClass('active');
+        $('#collector-addClass .test').addClass('active');
+        $('#collector-addClass .test').addClass('active');
+
+        expect(document.querySelector('#collector-addClass .test').className).to.equal("test active");
+      });
     });
   });
 });
