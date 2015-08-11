2da09d96d16dcf7d51e2b16c48804cd17b3bb8f7
Tom Bremer <tom@tbremer.com>
2015-08-11 01:06:22 -0500

    gh-pages first push

--- tests/hasClass/test.js
+++
@@ -1,9 +0,0 @@
-describe('hasClass', () => {
-  it('should return false', () => {
-    expect($('#collector-hasClass-false div').hasClass('test')).to.equal(false);
-  });
-
-  it('should return true', () => {
-    expect($('#collector-hasClass-true div').hasClass('test')).to.equal(true);
-  });
-});
